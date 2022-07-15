88074108a3918f3239fa3334ec4e49226a5daa11
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-07-14 23:31:45 -0500

    feat: add file create board in React

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 //Pages
 import SignUp from './pages/signup';
 import LogIn from './pages/login';
+import CreateBoard from './pages/createBoard';
 
 function App() {
   return (
---
+++ src/components/backgroundBoard/index.jsx
@@ -0,0 +1,24 @@
+import './backgroundBoard.scss'
+
+function BackgroundBoard() {
+  return (
+    <>
+        <ul className="board__ul">
+            <li className="board__li">
+                <button className="board__li__button"></button>
+            </li>
+            <li className="board__li">
+                <button className="board__li__button"></button>
+            </li>
+            <li className="board__li">
+                <button className="board__li__button"></button>
+            </li>
+            <li className="board__li">
+                <button className="board__li__button"></button>
+            </li>                        
+        </ul>
+    </>
+  )
+}
+
+export default BackgroundBoard;
