0da158f8aa6540397f6acb393512df960afafd28
David Pineda <dspinedao@gmail.com>
2022-07-16 23:11:21 -0500

    feat: Add page home in react

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import {BrowserRouter,Route, Routes} from "react-router-dom";
 import SignUp from './pages/signup';
 import LogIn from './pages/login';
 import MainBoard from './pages/board';
+import Home from './pages/home';
 //componets
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+     <NavBar />
       <Routes>
         <Route path= "/signUp" element={<SignUp />}/>
         <Route path= "/logIn" element={<LogIn />}/>
         <Route path= "/board" element={<MainBoard />}/>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
+
+
