e7e900ef9e65425b34c9f4398d2adccfb9bc50f4
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-07-20 20:31:56 -0500

    feat: add state for input and bottom of page createBoard

--- src/components/createBoard/index.jsx
+++ src/components/createBoard/index.jsx
@@ -1,10 +1,22 @@
-//import './createBoard.css'
+import {useState} from 'react';
 import BackgroundBoard from '../backgroundBoard';
 
 function CreateBoard ({modal, setModal}) {
 
+    const [task, setTask] = useState('');
+
     const handleModal = () => {
         setModal(!modal);
+    }
+
+    const handleInput = (e) => {
+        setTask(e.target.value);
+    }    
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        console.log(task);
+        setTask('');
     }
 
     return (
@@ -32,14 +44,14 @@ function CreateBoard ({modal, setModal}) {
                             </div>
                         </div>
 
-                        <form className='board__section__form'>
+                        <form className='board__section__form' onSubmit={handleForm}>
                             <div>
                                 <label className="board__section__label">                            
                                     Board Title <span className="board__section__span">*</span>
                                 </label> 
-                                <input className="board__section__input" type="text" required/>
+                                <input onChange={handleInput} className="board__section__input" type="text" value={task}/>
                                 <span className="board__section__span--2">👋 Board title is required</span>                        
-                                <button className="board__section__button">Create</button> 
+                                <button className="board__section__button" disabled={task.length > 3 ? '' : 'disabled'}>Create</button> 
                             </div>
                         </form>
                         
