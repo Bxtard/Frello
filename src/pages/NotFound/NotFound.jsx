99bcdb459748b39cea7cf71e7803fb5ef8fcad19
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-07-21 22:02:27 -0500

    feat: add page not found-404

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+
+const NotFound = () => {
+  return (    
+    <>
+      <div className="div">
+        <img className="div__img" src="..\img\notFound.jpg"/>
+      </div>
+      <section className="section">        
+        <img className="section__img1" src="..\img\logSing__footer__img1.png"/>
+        <img className="section__img2" src="..\img\logSing__footer__img2.png"/>
+      </section>
+    </>
+  )
+}
+
+export default NotFound;
