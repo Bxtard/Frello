9e8c9edf244a4642f7f00b251fd293cedf5c88ed
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-07-22 22:18:13 -0500

    feat: fix page 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,13 +2,23 @@
 const NotFound = () => {
   return (    
     <>
-      <div className="div">
-        <img className="div__img" src="..\img\notFound.jpg"/>
-      </div>
-      <section className="section">        
-        <img className="section__img1" src="..\img\logSing__footer__img1.png"/>
-        <img className="section__img2" src="..\img\logSing__footer__img2.png"/>
-      </section>
+      <main class="main">
+        <aside class="main__Description">
+          <h2 class="main__error">
+              UPS! PAGE NOT FOUND
+          </h2>        
+        </aside>
+        <section class="main__img">
+          <img src="..\img\goku_genkidama.png" alt="image reflector" />  
+          <p class="main__number">
+            404
+          </p>                  
+        </section>    
+        <footer className="footer__img">
+          <img className="footer__img1" src="..\img\logSing__footer__img1.png"/>
+          <img className="footer__img2" src="..\img\logSing__footer__img2.png"/>
+        </footer>
+      </main>
     </>
   )
 }
